3ce7b9d9357a1ebab02595f6ff2494daea56b272
TianchantgZhang <lesdanita@outlook.com>
2020-04-30 18:47:48 -0400

    Added location service

--- src/main/java/demo/domain/LocationRepostory.java
+++ src/main/java/demo/domain/LocationRepostory.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface LocationRepostory extends JpaRepository<Location, Long> {
     Page<Location> findByRunnerMovementType(@Param("movementType") Location.RunnerMovementType movementType, Pageable pageable);
---
+++ src/main/java/demo/service/impl/LocationServiceImpl.java
@@ -0,0 +1,34 @@
+package demo.service.impl;
+
+import demo.domain.Location;
+import demo.domain.LocationRepostory;
+import demo.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class LocationServiceImpl implements LocationService {
+    private LocationRepostory locationRepostory;
+
+    @Autowired
+    public  LocationServiceImpl(LocationRepostory locationRepostory) {
+        this.locationRepostory = locationRepostory;
+    }
+
+    @Override
+    public List<Location> saveRunningLocation(List<Location> runningLocations) {
+        return locationRepostory.save(runningLocations);
+    }
+
+    @Override
+    public void deleteAll() {
+        locationRepostory.deleteAll();
+    }
+
+    @Override
+    public Page<Location> findByRunnerMovementType(String movementType, Pageable pageable) {
+        return locationRepostory.findByRunnerMovementType(Location.RunnerMovementType.valueOf(movementType), pageable);
+    }
+}
