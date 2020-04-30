7c419fadf011a67e8d9ba1c000846aba9c2e91e6
TianchantgZhang <lesdanita@outlook.com>
2020-04-30 18:52:55 -0400

    fixed location service

--- src/main/java/demo/service/LocationService.java
+++ src/main/java/demo/service/LocationService.java
@@ -3,9 +3,11 @@ package demo.service;
 import demo.domain.Location;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface LocationService {
     List<Location> saveRunningLocation(List<Location> runningLocations);
 
