277f3cf51d9260117adfc06398230166e43e6566
TianchantgZhang <lesdanita@outlook.com>
2020-04-30 17:11:08 -0400

    Added location

---
+++ src/main/java/domain/Location.java
@@ -0,0 +1,56 @@
+package domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "RUNNING_LOCATIONS")
+public class Location {
+    enum GpsStatus {
+        EXCELLENT, OK, UNRELIABLE, BAD, NOTFIX, UNKNOW;
+    }
+
+    public enum RunnerMovementType {
+        STOPPED, IN_MOTION;
+
+        public boolean isMoving() {
+            return this != STOPPED;
+        }
+    }
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Embedded
+    @AttributeOverride(name = "bandMake",column = @Column(name = "unit_band_name"))
+    private final UnitInfo unitInfo;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="fmi", column = @Column(name="medical_fmi")),
+            @AttributeOverride(name="bfr", column = @Column(name="medical_bfr"))
+    })
+    private MedicalInfo medicalInfo;
+
+    private double latitude;
+    private double longitude;
+
+    private String heading;
+    private double gpsSpeed;
+    private GpsStatus gpsStatus;
+    private double odometer;
+    private double totalRunningTime;
+    private double totalIdleTime;
+    private double totalCalorieBurnt;
+    private String address;
+    private Date timestamp = new Date();
+    private String gearProvider;
+    private RunnerMovementType runnerMovementType = RunnerMovementType.STOPPED;
+    private String serviceType;
+
+    private Location(String runningId) {
+        this.unitInfo = new UnitInfo(runningId);
+    }
+
+}
---
+++ src/main/java/domain/MedicalInfo.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Data
+@Embeddable
+@RequiredArgsConstructor
+public class MedicalInfo {
+    private final long bfr;
+
+    private final long bmi;
+}
