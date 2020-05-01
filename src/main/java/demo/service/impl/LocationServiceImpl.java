78fb80070188f16627587c9d00bfb6b9b30b391c
TianchantgZhang <lesdanita@outlook.com>
2020-05-01 11:32:30 -0400

    Added location rest repository

--- src/main/java/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/demo/service/impl/LocationServiceImpl.java
@@ -1,7 +1,7 @@
 package demo.service.impl;
 
 import demo.domain.Location;
-import demo.domain.LocationRepostory;
+import demo.domain.LocationRepository;
 import demo.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,25 +12,25 @@ import java.util.List;
 
 @Service
 public class LocationServiceImpl implements LocationService {
-    private LocationRepostory locationRepostory;
+    private LocationRepository locationRepository;
 
     @Autowired
-    public  LocationServiceImpl(LocationRepostory locationRepostory) {
-        this.locationRepostory = locationRepostory;
+    public  LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
     }
 
     @Override
     public List<Location> saveRunningLocation(List<Location> runningLocations) {
-        return locationRepostory.save(runningLocations);
+        return locationRepository.save(runningLocations);
     }
 
     @Override
     public void deleteAll() {
-        locationRepostory.deleteAll();
+        locationRepository.deleteAll();
     }
 
     @Override
     public Page<Location> findByRunnerMovementType(String movementType, Pageable pageable) {
-        return locationRepostory.findByRunnerMovementType(Location.RunnerMovementType.valueOf(movementType), pageable);
+        return locationRepository.findByRunnerMovementType(Location.RunnerMovementType.valueOf(movementType), pageable);
     }
 }
